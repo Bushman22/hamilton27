195c5d55d5975d5291b6031f84ba303c87704505
Bushman22 <135259957+Bushman22@users.noreply.github.com>
2024-07-12 20:16:14 +0200

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-  ],
-};
 
 export default function RootLayout({
   children,
